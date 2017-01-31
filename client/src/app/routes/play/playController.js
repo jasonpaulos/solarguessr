67311052df26d76b955ca948224725830b24c2fb
jasonpaulos <jasongpaulos@gmail.com>
2017-01-31 01:36:12 -0700

    Fix bugs

--- client/src/app/routes/play/playController.js
+++ client/src/app/routes/play/playController.js
@@ -1,7 +1,7 @@
 var app = angular.module('solarguessr');
 
-app.controller('PlayController', ['$scope', '$timeout', 'modals', 'planets', 'score',
-	function ($scope, $timeout, modals, planets, score) {
+app.controller('PlayController', ['$scope', '$timeout', 'modals', 'planets', 'score', 'account',
+	function ($scope, $timeout, modals, planets, score, account) {
 		var actualLocation = {
 			planet: null,
 			location: {
@@ -127,46 +127,50 @@ app.controller('PlayController', ['$scope', '$timeout', 'modals', 'planets', 'sc
 				$scope.showScore = true;
 			}, 1500);
 			
-			score.postScore(user_score)
-				.then(function (res) {
-					
-				})
-				.catch(function (err) {
-					console.error(err);
-				});
+			if (account.isSignedIn()) {
+				score.postScore(user_score)
+					.then(function (res) {
+						
+					})
+					.catch(function (err) {
+						console.error(err);
+					});
+			}
 		}
 		
 		$scope.playAgain = function () {
-			var layers = viewer.scene.imageryLayers;
-			
-			actualLocation.planet = planets[Math.floor(Math.random() * planets.length)];
-			
-			var imageryProvider = Cesium.createTileMapServiceImageryProvider({
-				url: actualLocation.planet.url,
-                        	fileExtension: 'jpg'
-               		});
-			
-			layers.removeAll()
-			layers.addImageryProvider(imageryProvider);
-	
-			actualLocation.location.lon = getRandomArbitrary(-180.0, 180.0);
-	                actualLocation.location.lat = 180.0 / Cesium.Math.PI * Math.acos(getRandomArbitrary(-1.0, 1.0));
-	                var height = 500000.0;
-	                var heading = getRandomArbitrary(0, 2*Cesium.Math.PI);
-	
-	                camera.setView({
-	                        destination: Cesium.Cartesian3.fromDegrees(actualLocation.location.lon, actualLocation.location.lat, height),
-	                        orientation: {
-	                                heading: heading,
-	                                pitch: -Cesium.Math.PI_OVER_TWO,
-	                                roll: 0.0
-	                        }
-	                });
-
 			$scope.score = null;
-			$scope.showScore = false;
-			$scope.canGuess = true;
-			canLook = true;
+                        $scope.showScore = false;
+                        $scope.canGuess = true;
+                        canLook = true;
+			
+			$timeout(function () {
+				var layers = viewer.scene.imageryLayers;
+	
+	                        actualLocation.planet = planets[Math.floor(Math.random() * planets.length)];
+	
+	                        var imageryProvider = Cesium.createTileMapServiceImageryProvider({
+	                                url: actualLocation.planet.url,
+	                                fileExtension: 'jpg'
+	                        });
+	
+	                        layers.removeAll()
+	                        layers.addImageryProvider(imageryProvider);
+	
+	                        actualLocation.location.lon = getRandomArbitrary(-180.0, 180.0);
+	                        actualLocation.location.lat = 180.0 / Cesium.Math.PI * Math.acos(getRandomArbitrary(-1.0, 1.0));
+	                        var height = 500000.0;
+	                        var heading = getRandomArbitrary(0, 2*Cesium.Math.PI);
+	
+	                        camera.setView({
+	                                destination: Cesium.Cartesian3.fromDegrees(actualLocation.location.lon, actualLocation.location.lat, height),
+	                                orientation: {
+	                                        heading: heading,
+	                                        pitch: -Cesium.Math.PI_OVER_TWO,
+	                                        roll: 0.0
+	                                }
+	                        });
+			}, 500);
 		};
 		
 		$scope.actualLocation = actualLocation;
